ba58568a384c0412c2d3a5273ccc173e324e1a80
Fardin Alam <P2532696@my365.dmu.ac.uk>
2021-03-12 20:15:45 +0000

    testingorder commit

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrder
+    {
+
+        public string ProductDescription { get; set; }
+        public bool Item { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Address { get; set; }
+        public int Customer { get; set; }
+        public int OrderId { get; set; }
+        public decimal Price { get; set; }
+        public char Colour { get; set; }
+
+        public string Valid(string Pdescription)
+        {
+            if (Pdescription.Length < 1)
+            {
+                return "Product Description cannot be blank";
+            }
+            else if (Pdescription.Length > 50)
+            {
+                return "Product Description cannot exceed 50 chars";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ Testing3/tstOrder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstOrder
+    {
+        //good test data
+        private string Pdescription = "Random Description";
+        int OrderId = 3;
+        int CustomerNo = 36;
+        string OrderDate = DateTime.Now.Date.ToString();
+        char ItemColour = 'r';
+        bool Availability = true;
+        string CustomerAddress = "LE2 1PQ";
+        decimal ItemPrice = 10;
+
+
+        [TestMethod]
+        public void ProductDescriptionOK()
+        {
+            clsOrder AOrder = new clsOrder();
+            string TestData = Pdescription;
+            AOrder.ProductDescription = TestData;
+            Assert.AreEqual(AOrder.ProductDescription, TestData);
+        }
+
+        [TestMethod]
+        public void AvailabilityOK()
+        {
+            clsOrder AOrder = new clsOrder();
+            Boolean TestData = true;
+            AOrder.Item = TestData;
+            Assert.AreEqual(AOrder.Item, TestData);
+        }
+        [TestMethod]
+        public void OrderDateOK()
+        {
+            clsOrder AOrder = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+            AOrder.OrderDate = TestData;
+            Assert.AreEqual(AOrder.OrderDate, TestData);
+        }        [TestMethod]
+        public void CustomerAddressOK()
+        {
+            clsOrder AOrder = new clsOrder();
+            String TestData = "Delivery Address";
+            AOrder.Address = TestData;
+            Assert.AreEqual(AOrder.Address, TestData);
+        }        [TestMethod]
+        public void CustomerNumberOK()
+        {
+            clsOrder AOrder = new clsOrder();
+            int TestData = 121;
+            AOrder.Customer = TestData;
+            Assert.AreEqual(AOrder.Customer, TestData);
+        }        [TestMethod]
+        public void OrderIdOK()
+        {
+            clsOrder AOrder = new clsOrder();
+            int TestData = 134;
+            AOrder.OrderId = TestData;
+            Assert.AreEqual(AOrder.OrderId, TestData);
+        }
+        [TestMethod]
+        public void ItemPriceOk()
+        {
+            clsOrder AOrder = new clsOrder();
+            decimal TestData = 35;
+            AOrder.Price = TestData;
+            Assert.AreEqual(AOrder.Price, TestData);
+        }
+
+        [TestMethod]
+        public void ItemColourOk()
+        {
+            clsOrder AOrder = new clsOrder();
+            char TestData = 'r';
+            AOrder.Colour = TestData;
+            Assert.AreEqual(AOrder.Colour, TestData);
+
+        }
+
+    }
+}
