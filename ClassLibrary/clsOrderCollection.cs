f54d044122fa641eb3b49220f673ccbfbe99c101
Fardin Alam <P2532696@my365.dmu.ac.uk>
2021-04-23 00:23:13 +0100

    clsOrderCollection added

---
+++ ClassLibrary/clsOrderCollection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsOrderCollection
+    {
+        private List<clsOrder> mOrderList;
+
+        public List<clsOrder> OrderList
+        {
+            get
+            {
+                return mOrderList;
+            }
+            set
+            {
+                mOrderList = value;
+
+            }
+        }
+
+
+
+
+        public int Count
+        {
+            get
+            {
+                return mOrderList.Count;
+            }
+            set
+            {
+                //later
+            }
+        }
+
+
+
+
+
+
+        public clsOrder ThisOrder { get; set; }
+    }
+}
---
+++ Testing3/tstOrderCollection.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System.Collections.Generic;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstOrderCollection
+    {
+       [TestMethod]
+       public void InstanceOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            Assert.IsNotNull(AllOrders);
+        }
+
+
+        [TestMethod]
+        public void OrderListOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            List<clsOrder> TestList = new List<clsOrder>();
+            clsOrder TestItem = new clsOrder();
+            TestItem.OrderID = 1;
+            TestItem.CustomerNo = 10;
+            TestItem.CustomerAddress = "39 Random Street";
+            TestItem.ItemColour = "red";
+            TestItem.ItemPrice = 7;
+            TestItem.OrderDate = DateTime.Now.Date;
+            TestItem.ProductDescription = "white shirt";
+            TestItem.Availability = true;
+            TestList.Add(TestItem);
+            AllOrders.OrderList = TestList;
+            Assert.AreEqual(AllOrders.OrderList, TestList);
+        }
+
+
+        [TestMethod]
+        public void CountPropertyOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            Int32 SomeCount = 0;
+            AllOrders.Count = SomeCount;
+            Assert.AreEqual(AllOrders.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisOrderPropertyOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            clsOrder TestItem = new clsOrder();
+            TestItem.OrderID = 1;
+            TestItem.CustomerNo = 10;
+            TestItem.CustomerAddress = "39 Random Street";
+            TestItem.ItemColour = "red";
+            TestItem.ItemPrice = 7;
+            TestItem.OrderDate = DateTime.Now.Date;
+            TestItem.ProductDescription = "white shirt";
+            TestItem.Availability = true;
+            AllOrders.ThisOrder = TestItem;
+            Assert.AreEqual(AllOrders.ThisOrder, TestItem);
+        }
+
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            List<clsOrder> TestList = new List<clsOrder>();
+            clsOrder TestItem = new clsOrder();
+            TestItem.OrderID = 1;
+            TestItem.CustomerNo = 10;
+            TestItem.CustomerAddress = "39 Random Street";
+            TestItem.ItemColour = "red";
+            TestItem.ItemPrice = 7;
+            TestItem.OrderDate = DateTime.Now.Date;
+            TestItem.ProductDescription = "white shirt";
+            TestItem.Availability = true;
+            TestList.Add(TestItem);
+            AllOrders.OrderList = TestList;
+            Assert.AreEqual(AllOrders.Count, TestList.Count);
+        }
+
+
+
+        [TestMethod]
+        public void TwoRecordsPresent()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            Assert.AreEqual(AllOrders.Count, 2);
+        }
+
+    }
+}
